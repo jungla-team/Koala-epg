98b0a5c9764920c63de4f9a447fac5fbb50261ec
loznic89 <44941508+loznic89@users.noreply.github.com>
2020-07-03 22:11:59 +0200

    Add files via upload

--- grabber42.sh
+++ grabber42.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd /storage/downloads
 wget http://tropical.jungle-team.online/tvheadend/grabber42.tar
-tar xvf /storage/grabber42.tar -C /
-rm -f /storage/grabber42.tar
+tar xvf /storage/downloads/grabber42.tar -C /
+rm -f /storage/downloads/grabber42.tar
 systemctl restart service.tvheadend42
 
 
