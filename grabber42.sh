855a461c4dcb00173c24aafb4eef9954e384ef84
loznic89 <44941508+loznic89@users.noreply.github.com>
2020-07-03 23:11:08 +0200

    Delete grabber42.sh

--- grabber42.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -o grabber42.tar http://tropical.jungle-team.online/tvheadend/grabber42.tar
-tar xvf /storage/grabber42.tar -C /
-rm -f /storage/grabber42.tar
-systemctl restart service.tvheadend42
-
-
